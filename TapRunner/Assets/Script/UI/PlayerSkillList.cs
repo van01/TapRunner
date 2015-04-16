d763c433ca10f901beac223877b7e16bc88b6f20
van01 <van_01@hotmail.com>
2015-04-16 18:40:10 +0900

    하단 매뉴 작업 진행

---
+++ TapRunner/Assets/Script/UI/PlayerSkillList.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSkillList : MonoBehaviour {
+
+	public GameObject m_prefab;
+
+	private bool m_isReposition = false;
+
+	// Use this for initialization
+	void Start () {
+		if (m_prefab != null) {
+
+			UIGrid grid = GetComponentInChildren<UIGrid>();
+
+			NGUITools.AddChild(grid.gameObject, m_prefab);
+			NGUITools.AddChild(grid.gameObject, m_prefab);
+			NGUITools.AddChild(grid.gameObject, m_prefab);
+			NGUITools.AddChild(grid.gameObject, m_prefab);
+
+			grid.Reposition ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ TapRunner/Assets/Script/UI/ResizeParentWidth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeParentWidth : MonoBehaviour {
+
+	public int offsetWidth = 4;
+
+	// Use this for initialization
+	void Start () {
+		float fWidth = GetComponentInParent<UIPanel> ().width;
+		GetComponent<UISprite> ().width = (int)fWidth - offsetWidth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
